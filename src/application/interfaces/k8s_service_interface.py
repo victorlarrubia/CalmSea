2be032a1d7f4f769533c184e17afe0484e803a9a
VitorDie <vitordie@protonmail.ch>
2026-02-06 14:36:12 -0300

    TestGetResourceDetailsCommand test_execute_should_return_resource_details (test pass)

--- src/application/interfaces/k8s_service_interface.py
+++ src/application/interfaces/k8s_service_interface.py
@@ -1,8 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import List
+from typing import List, Dict, Any  # <--- Adicionamos Dict e Any aqui!
 
 class K8sServiceInterface(ABC):
     @abstractmethod
     def list_resources(self, resource_type: str, namespace: str) -> List[str]:
         """Lista nomes de recursos de um tipo específico."""
+        pass
+
+    @abstractmethod
+    def get_resource_details(self, resource_type: str, name: str, namespace: str) -> Dict[str, Any]:
+        """Obtém os detalhes completos (YAML/JSON) de um recurso."""
         pass
